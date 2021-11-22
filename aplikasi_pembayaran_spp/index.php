e9bede0dd65ed75414bd854e8fbbd976616dd371
apaansihzan <54340241+apaansihzan@users.noreply.github.com>
2021-11-22 11:31:53 +0700

    Add files via upload

---
+++ aplikasi_pembayaran_spp/index.php
@@ -0,0 +1,101 @@
+<?php
+if (!session_id()) session_start();
+require_once 'Proses.php';
+
+// buat object
+$proses = new Proses;
+
+// cek session, apabila sudah ada maka akan diarahkan ke halaman beranda
+if (isset($_SESSION['id'])) {
+    if ($_SESSION['level'] == "Admin") {
+        header('Location: includes/admin/');
+    } else {
+        // kita belum buat
+        header('Location: petugas/');
+    }
+}
+
+// ketika tombol masuk diklik maka jalankan kode berikut
+if (isset($_POST['masuk'])) {
+    // menghindari sql injection
+    $username = $proses->konek->real_escape_string($_POST['username']);
+    $password = $proses->konek->real_escape_string(sha1($_POST['password']));
+
+    $masuk = $proses->loginPetugas($username, $password);
+
+    if ($masuk->num_rows > 0) {
+        $data = mysqli_fetch_assoc($masuk);
+
+        if ($data['level'] == "Admin") {
+            header('Location: includes/admin');
+            $_SESSION['id'] = $data['id_petugas'];
+            $_SESSION['level'] = $data['level'];
+        } else {
+            header('Location: petugas');
+            $_SESSION['id'] = $data['id_petugas'];
+            $_SESSION['level'] = $data['level'];
+        }
+    } else {
+        $_SESSION['error'] = "Username atau password tidak valid";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplikasi Pembayaran SPP</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="#224422" fill-opacity="1" d="M0,288L1440,192L1440,0L0,0Z"></path>
+</svg>
+
+
+
+<body>
+
+    <div class="row">
+        <div class="col-8">
+            <div class="row py-5 px-5">
+                <div class="col-4 ">
+                    <h1 class="text-end" style="width:100px; ">APLIKASI PEMBAYARAN SPP </h1> 
+                </div>
+                <div class="col-4 ">
+                    <img src="assets/IMG1.svg" alt="" class="" style="width:500px; ">
+                </div>
+            </div>
+        </div>
+        <div class="col-4 py-7 px-10">
+            <h2>Silahkan Masuk</h2>
+            <?php
+            if (isset($_SESSION['error'])) {
+                echo '<span style="color:red;">' . $_SESSION['error'] . '</span>';
+            }
+            ?>
+            <form method="post" action="" 5complete="off">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" name="username" id="username" placeholder="Username" class="form-control" style="width:300px; border-radius:100px">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" id="password" placeholder="Password" class="form-control" style="width:300px; border-radius:100px">
+                <input type="submit" name="masuk" value="Masuk" class="btn btn-primary mt-3" style="border-radius: 40px;">
+            </form>
+        </div>
+    </div>
+
+</body>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="#224422" fill-opacity="1" d="M0,32L80,64C160,96,320,160,480,170.7C640,181,800,139,960,106.7C1120,75,1280,53,1360,42.7L1440,32L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+</svg>
+<!-- link js bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</html>
